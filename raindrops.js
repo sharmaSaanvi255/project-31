12903e010a09b8c24f0a4d0b8fea1c1922140220
sharmaSaanvi255 <72978396+sharmaSaanvi255@users.noreply.github.com>
2021-02-06 13:02:17 +0530

    Add files via upload

---
+++ raindrops.js
@@ -0,0 +1,10 @@
+class Raindrops {
+    constructor(x,y){
+    var options = {
+        restitution : 0.8,
+        friction : 1.0,
+        density : 1.0
+    }
+    this.body=bodies.circle();
+} 
+}
